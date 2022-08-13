4977e668680231f574be80def953f69c61994579
Joshua Godinez <jgodi96@gmail.com>
2022-08-13 08:52:49 -0700

    website

---
+++ src/routes/home.component.jsx
@@ -0,0 +1,12 @@
+import IntroHeader from "../components/intro-header/intro-header.component"
+
+const Home = () =>{
+    return(
+        <>
+        
+        <IntroHeader/>
+        </>
+    )
+}
+
+export default Home
