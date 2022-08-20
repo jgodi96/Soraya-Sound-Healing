6ec40954c849b833e99efd02a9f7af9d7efae759
Isaiah Guardado <gisaiah416@gmail.com>
2022-08-20 16:10:21 -0700

    Added GET test Route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 
+
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use('/', require('./routes/testRoute.js'))
 
 app.listen(port, () => console.log(`Running on PORT ${port}`))
