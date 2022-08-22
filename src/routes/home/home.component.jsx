945a8c26e3605f7f5f7c83ea0a84f1801e2f12f0
Joshua Godinez <jgodi96@gmail.com>
2022-08-21 20:19:34 -0700

    added some style to home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,35 +1,42 @@
-import './home.styles.scss'
-import MainImage from '../../assets/Main (1).jpg'
-import HowItWorks from './how-it-works/how-it-works.components';
-import {FiYoutube} from 'react-icons/fi'
-import {FiTwitter} from 'react-icons/fi'
-import {FiInstagram} from 'react-icons/fi'
+import "./home.styles.scss";
+import MainImage from "../../assets/Main (1).jpg";
+import HowItWorks from "./how-it-works/how-it-works.components";
+import { FiYoutube } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
 
 const Home = () => {
   return (
     <>
-    <div className="home-container">
-    <div className="description-content">
-      <h1>Soraya Sound Healing</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <button>Schedule</button>
-      <div className="icons-container">
-      <FiYoutube/>
-      <FiTwitter/>
-      <FiInstagram/>
+      <div className="home-container">
+        <div className="description-content">
+          <h1>Soraya Sound Healing</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="home-button-container">
+            <button className="home-schedule-button">Schedule</button>
+            <button className="home-learn-more-button">Learn More</button>
+          </div>
+
+          <div className="icons-container">
+            <FiYoutube className="yt" />
+            <FiTwitter className="tw" />
+            <FiInstagram className="ig"/>
+              
+          
+          </div>
+        </div>
+        <img className="hero" src={MainImage} alt="" />
+      </div>
       
-    </div>
-
-    </div>
-    <img className="hero" src={MainImage} alt=""/>
-    
-
-    </div>
-    <HowItWorks/>
-
-
     </>
-
   );
 };
 
