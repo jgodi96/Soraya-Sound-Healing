b7f3c5c3756206ae94bf5d691388cdb4fe153a1a
Joshua Godinez <jgodi96@gmail.com>
2022-08-22 15:46:58 -0700

    fixed home styling

--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -1,33 +1,33 @@
-import {useState} from 'react'
+//import {useState} from 'react'
 import { Outlet,Link } from "react-router-dom"
 import Logo from '../logo/logo.component'
 import './nav-bar.styles.scss'
 
 const NavBar = () => {
 
-    const [navBarScroll,setNavBarScroll] = useState(false)
+    // const [navBarScroll,setNavBarScroll] = useState(false)
 
-    const changeBackground = () => {
+    // const changeBackground = () => {
 
-        if(window.scrollY >= 1 ){
-            setNavBarScroll(true)
-        }
-        else{
-            setNavBarScroll(false);
-        }
-    }
-    window.addEventListener('scroll',changeBackground)
+    //     if(window.scrollY >= 1 ){
+    //         setNavBarScroll(true)
+    //     }
+    //     else{
+    //         setNavBarScroll(false);
+    //     }
+    // }
+    // window.addEventListener('scroll',changeBackground)
     return (
         <>
-        <div className={navBarScroll ? 'nav-container active' : 'nav-container'}>
+        <div className='nav-container'>
 
             <Logo></Logo>
 
             <div className="link-container">
-            <Link to= '/'>Home</Link>
-            <Link to= '/schedule'>Schedule</Link>
-            <Link to= '/contact'>Contact</Link>
-            <Link to= '/about'>About</Link>
+            <Link to='/'>Home</Link>
+            <Link to='/schedule'>Schedule</Link>
+            <Link to='/contact'>Contact</Link>
+            <Link to='/about'>About</Link>
             </div>
 
 
