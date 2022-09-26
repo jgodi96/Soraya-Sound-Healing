bd0954cd27c294b40c3033abc7f1bf3fec7b30ce
Isaiah Guardado <gisaiah416@gmail.com>
2022-09-26 00:56:34 -0700

    Added Middleware

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -5,7 +5,10 @@ const getTest = (req, res) => {
 }
 
 const setTest = (req, res) => {
-    console.log(req.body)
+    if (!req.body.text){
+        res.status(400);
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message: 'Set Goal'})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,5 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/testRoute.js'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Running on PORT ${port}`.green))
